b73b6f155fe539b980813ef6be4504d9f27a5411
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-03-30 00:10:46 +0900

    feat: tailwind css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div></div>
-      <h1>Vite + React</h1>
+      <h1 className="text-red-200">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
