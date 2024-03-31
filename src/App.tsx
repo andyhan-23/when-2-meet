c36fdb6b090365341ee03fdee0c9a69b4c485beb
andyhan-23 <andyhanjaehyeok.1123@gmail.com>
2024-03-31 13:27:59 +0900

    feat: layout 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { router } from "./routes";
 
 function App() {
   return (
-    <div className="h-dvh overflow-hidden relative desktop:max-w-[400px] desktop:mx-auto">
+    <div className="h-dvh flex overflow-hidden relative desktop:max-w-[400px] desktop:mx-auto">
       <RouterProvider router={router} />
     </div>
   );
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,19 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import { PATH } from "./constatns";
 import { MainPage, LoginPage, EventPage } from "@/pages";
+import Layout from "./layout";
 
 const router = createBrowserRouter([
   {
     path: PATH.main,
-    element: <MainPage />,
+    element: <Layout />,
+    children: [{ path: PATH.main, element: <MainPage /> }],
   },
   {
     path: PATH.login,
-    element: <LoginPage />,
+    element: <Layout />,
+    children: [{ path: PATH.login, element: <LoginPage /> }],
   },
   {
     path: PATH.event,
-    element: <EventPage />,
+    element: <Layout />,
+    children: [{ path: PATH.event, element: <EventPage /> }],
   },
 ]);
 
